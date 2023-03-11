a7c186109a35c44a21edeca55c23a4fbd7a45efa
Shobhitverma9 <86529341+Shobhitverma9@users.noreply.github.com>
2023-03-11 11:54:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+import fetch from "node-fetch";
 const morgan = require('morgan');
 const PORT = process.env.PORT || 3030;
 
